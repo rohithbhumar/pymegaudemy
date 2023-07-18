0eacf29948d204e299b576878d4d1709e4b8c933
Rohit B <robinfrancis40@gmail.com>
2023-07-18 14:43:37 -0400

    changed the var name

--- leet.py
+++ leet.py
@@ -18,3 +18,31 @@ def search_index(target):
     return len(nums)
 
 print(search_index(97))
+
+# Given dictionary - LinkedIn ex:
+my_dict = {
+    "key1": [1, 2, 3],
+    "key2": [4, 5, 6],
+    "key3": "hello",
+    "key4": [7, 8, 9, 10]
+}
+
+# Threshold value
+threshold = 14
+
+# Initialize an empty dictionary to store the filtered key-value pairs
+new_dict = {}
+
+# Iterate through the items (key-value pairs) of the given dictionary
+for key, value in my_dict.items():
+    # Check if the value is a list and all elements in the list are integers
+    if isinstance(value, list) and all(isinstance(x, int) for x in value):
+        # Calculate the sum of the elements in the list
+        sum_of_list = sum(value)
+        # Check if the sum is greater than the threshold
+        if sum_of_list > threshold:
+            # Add the key-value pair to the new dictionary
+            new_dict[key] = value
+
+# Print the new dictionary
+print(new_dict)
